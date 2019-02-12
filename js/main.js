3d57c9c4d96079eac4d74ae7143eafbbee643d3c
ImraanMeyer <imraan.meyer97@gmail.com>
2019-02-12 14:52:18 +0200

    initial

---
+++ js/main.js
@@ -0,0 +1,16 @@
+console.log("Hello JavaScript");    
+
+let name    =   " Imraan";
+    surname =   " Meyer"
+    age     =   " 21";
+    gender  =   " Male";
+    message =   "Good Morning";
+    cake    =   "Chocolate Moouse";
+    layers  =   3;
+    price   =   200000;
+
+console.log(name + surname + age + gender);
+alert (message + name);
+prompt("")
+
+console.log("Hello" + name + surname, "Ive managed to bake a " + cake, "cake with " + layers  + " layers and it costs R" + price);
---
+++ js/nate-task.js
@@ -0,0 +1,30 @@
+// Create a message where anyone can enter your site (prompt) and they can get in the sight. Must give a website where it says welcome mr or uncle
+// Create var valentines special restaurant, time, location, action (sunset cruise), gift
+
+// - Name
+// - "Welcome Message"
+
+// - Alert 
+// - "Welldone for completing, your VDAY is gonna be lit"
+
+// - Once finish reading they must get a alert
+
+let message    = prompt ("Please fill in your name ..");
+    restaurant = "Ocean Basket";
+    time       = "8:00 PM";
+    area       = "V&A Waterfront";
+    action     = "walk on the beach"
+    gift       = "roses"
+    ammount    = 100;
+
+    switch (message) {
+        case message:
+            console.log("Welcome " + message);
+            break;
+    
+        default:
+            break;
+    }
+
+    console.log 
+        ("Please join me at " + res+taurant + " for the best time of your life at the " + area + " at " + time + " followed by a " + action + " so I can give you a " + ammount + " " + gift);
