3f788505ade0755b588c4bdf74717eaab887199f
ImraanMeyer <imraan.meyer97@gmail.com>
2019-02-20 10:59:32 +0200

    added looping and functions .js

---
+++ js/functions.js
@@ -0,0 +1,97 @@
+ /* === What is a function? === */
+
+ // A function is a group of instructions that performs a particular task.
+
+ function sayHello() {
+     console.log("Hello");
+ }
+
+ console.log("Start of Program"); 
+ sayHello();    //calling the function
+ console.log("End of Program");
+
+ // Declaring a function
+
+ // Creating a function
+
+
+
+
+
+ // The keyword return indicates that a return value will be given, which ia specified immediatley after the word.
+
+function sayHello() {
+     return "Hello";
+}
+
+console.log("Start of Program"); // just for example purposes
+let result = sayHello();
+console.log(result);
+console.log("End of Program");
+
+/* === 
+
+// Declare myFunction
+function myFunction() {
+    // calculate return value
+    // ... 
+    return returnValue;
+}
+
+
+// Get return value from myFunction
+let value = myFunction();
+// ...
+
+=== */
+
+// creating a function
+function message() {
+    return "Good Morning";
+}
+console.log(message());
+
+/* === 
+    function myFinction () {
+        // No value returned in this function
+    }
+
+    var result = myFunction();
+
+    console.log(result); // Will be undefined
+
+=== */
+
+// A function  that returns no value is sometimes called a proedure
+
+
+// create a function with function name sayHello and parametere name
+// declare a variable inside the function the variable name to be message
+// the function should display Hello, name of the person
+
+console.log("start");
+
+function sayHello(firstName, lastName) {
+    let message = "Hello, " + firstName + lastName + "!";
+    return message;
+}
+
+console.log(sayHello("Steve"," Rogers" ));
+
+
+console.log("end");
+
+/* --------------- END ----------- */
+
+message = "Hello there\n"
+console.log(message + "JavaScript is not JAVA" .length);
+
+
+
+let animal      = ["cat", "dog", "cow"];
+    // caseUp      = animal.toUpperCase(animal);
+    // caseDown    = animal.toLocaleLowerCase(animal);
+    
+    console.log(animal[2]);
+    // console.log(animal === "cat" + caseUp);
+    // console.log(caseUp + " " + caseDown);
---
+++ js/looping.js
@@ -0,0 +1,60 @@
+console.log("start program");
+console.log (1, 2, 3, 4, 5, 6, 7,  8, 9, 10);
+console.log("end program");
+
+// will be looking at a 'while' & 'for' loop
+// while loops - loops through a block of code while a specified condition is true
+// for loops -  loops through a block of code a number of times
+
+let number = 1;
+while (number <= 10) {
+    console.log (number);
+    number++;
+}
+
+// basic syntax 
+
+/* ===
+while (condition) {
+    // code to run while the condition is true
+}
+
+=== */
+
+// Before each loop iteration, the condition in parenthisis is evaluated to determine whether its true or not. The code associated with a loop is called the body
+
+// if the condition true, the code in the while loops body runs. Afterward, the condition is re-evealuated to see if its still true or not. The cycle continues!
+
+// if the condition is fale, the code in the loop stops running or doesnt run.
+
+/* ===== For Loop ==== */
+
+// Youll often need to write loops with conditions that are based on what happens in the loop body, like in our example. JS offers another loop type to account for this: the 'for' loop
+
+/* ====
+
+for  (initialization; condition; final-expression) {
+    // code to run when condition is true
+}
+
+==== */ 
+
+// This is a little more complicated than the while loop syntax:
+
+// Initializatio only happens once, when the code first kicks off
+
+// The condition is evaluated once before the loop runs each time. If it's true, the code runs. If not, the code doesnt run.
+
+// The final expression is evaulated after the loop runs each time. It's often used to update a counter variable, as we saw in the while loop example. 
+
+for (let counter = 1; counter <= 5; counter+1) {
+    console.log(counter);
+}
+
+/* === which loop should i use? === */
+
+let letter = "";
+while (letter !=="X") {
+    letter = prompt("Type any letter of X to exit:");
+    console.log(letter);
+}
